84849bb0d0b34f61872c58567056c88b6ca3fb3e
Athulya Thomas <athulyathomas2398@gmail.com>
2024-11-25 20:06:57 +0530

    clone- landing page of project using react js

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import './App.css'
+
 
 
 
@@ -14,14 +14,14 @@ function Header() {
     <>
      <Navbar className='nav'  style={{backgroundColor:"#323232"}} data-bs-theme="dark">
         <Container>
-            <img src="https://mobirise.com/extensions/wowslider/assets/images/logo.png" style={{width:"30px"}} alt="" />
-          <Navbar.Brand href="#home" className='fs-5 fw-bold'>MOBIRISE</Navbar.Brand>
+            <img src="https://mobirise.com/extensions/wowslider/assets/images/logo.png" style={{width:"15px"}} alt="" />
+          <Navbar.Brand href="#home" style={{fontSize:"20px"}}  className=' fw-bold'>MOBIRISE</Navbar.Brand>
           <Nav className="ms-auto">
-            <Nav.Link href="#home" style={{color:"white",fontWeight:"bolder"}}>HOME</Nav.Link>
-            <Nav.Link href="#demo"style={{color:"white",fontWeight:"bolder"}}>DEMO</Nav.Link>
+            <Nav.Link href="#home" style={{color:"white",fontWeight:"bolder" ,fontSize:"15px"}}>HOME</Nav.Link>
+            <Nav.Link href="#demo"style={{color:"white",fontWeight:"bolder",fontSize:"15px"}}>DEMO</Nav.Link>
             
           </Nav>
-          <button className='btn   ms-3 fw-bolder' style={{border:"2px solid white" ,color:"white",fontSize:"13px"}}>BUY NOW</button>
+          <button className='btn   ms-3 fw-bolder' style={{border:"2px solid white" ,color:"white",fontSize:"15px"}}>BUY NOW</button>
           
         </Container>
       </Navbar>
